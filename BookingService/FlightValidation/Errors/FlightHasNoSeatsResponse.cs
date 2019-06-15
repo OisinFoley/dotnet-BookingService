70ab364bee90ec4ca24845654b381aea8c90ffb1
OisinFoley <oisinfoley@yahoo.co.uk>
2019-06-15 18:34:49 +0100

    Added FlightValidation class which checks (through the use of an Autorest-generated client) that there are available seats for a given flight in the FlightService whenever a POST request comes in for a new booking. Updated startup class and BookingController to inject this new dependency, and updated the unit tests and markdown file to reflect the new functionality.

---
+++ BookingService/FlightValidation/Errors/FlightHasNoSeatsResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace BookingService.FlightValidation.Errors
+{
+    public static class FlightHasNoSeatsResponse
+    {
+        public const string ErrorMessage = "flight_has_no_remaining_seats_available";
+    }
+}
---
+++ BookingService/FlightValidation/FlightValidator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using BookingService.FlightValidation;
+using FlightService.Client;
+using FlightService.Client.Models;
+using Newtonsoft.Json;
+
+namespace BookingService.Extensions
+{
+    public class FlightValidator: IFlightValidator
+    {
+        protected readonly IFlightServiceAPI m_FlightServiceClient;
+
+        public FlightValidator(string uri)
+        {
+            m_FlightServiceClient = new FlightServiceAPI
+            {
+                BaseUri = new Uri(uri)
+            };
+        }
+
+        public async Task<bool> HasSeatsAvailable(Guid id)
+        {
+            var flightGetHttpResponse = await m_FlightServiceClient.Get1WithHttpMessagesAsync(id);
+            var flightGetResponseString = await flightGetHttpResponse.Response.Content.ReadAsStringAsync();
+            FlightResponse flightGetResponse = JsonConvert.DeserializeObject<FlightResponse>(flightGetResponseString);
+
+            return flightGetResponse.Flight.AvailableSeats > 0;
+        }
+    }
+}
